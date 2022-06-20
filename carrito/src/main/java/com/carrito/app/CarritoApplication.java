625ea5315ceb8089f55659f20ad0a8dab0371d00
Fede Buchet <fedebuchet17@gmail.com>
2022-06-20 02:04:46 -0300

    cambios minimos

--- carrito/src/main/java/com/carrito/app/CarritoApplication.java
+++ carrito/src/main/java/com/carrito/app/CarritoApplication.java
@@ -1,22 +1,13 @@
 package com.carrito.app;
 
-import com.carrito.app.service.interfaces.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CarritoApplication implements CommandLineRunner {
+public class CarritoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarritoApplication.class, args);
-	}
-
-	@Autowired
-	private UserService userService;
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println(userService.findByEmailDto("fedebuchet17@gmail.com").get().getId());
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CarritoApplication.class, args);
+    }
+	
 }
